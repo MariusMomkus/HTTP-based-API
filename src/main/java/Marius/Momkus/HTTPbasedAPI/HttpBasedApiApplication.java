5559bb51fa011eda198de5b3d3e490950c9edd0d
Marius <72971009+MariusMomkus@users.noreply.github.com>
2023-06-08 14:12:26 +0300

    Add files via upload

---
+++ src/main/java/Marius/Momkus/HTTPbasedAPI/HttpBasedApiApplication.java
@@ -0,0 +1,13 @@
+package Marius.Momkus.HTTPbasedAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpBasedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpBasedApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Marius/Momkus/HTTPbasedAPI/HttpBasedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Marius.Momkus.HTTPbasedAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpBasedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
